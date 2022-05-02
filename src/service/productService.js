3ec5c591c627164c9c2ca54bdae7354d37aab352
babinobass <69868198+babinobass@users.noreply.github.com>
2022-05-01 23:06:42 -0300

    fix padding container featured

--- src/service/productService.js
+++ src/service/productService.js
@@ -1,10 +1,18 @@
 
+/* 
+* La funcion getProductFeatured recibe una funcion como callback para devolver el featured
+*/
 export async function getProductFeatured(fnt) {
     await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/v1/products`)
         .then(response => response.json())
         .then(newData => fnt(newData))
 }
 
+
+/* 
+* La funcion addToCart recibe un item product como argumento y lo guarda en el localStorage
+* si la orden no fue creada, la crea con el item, sino agrega el item a la orden
+*/
 export function addToCart(item) {
     const order = JSON.parse(localStorage.getItem('order')) ?? false
     if (order) {
